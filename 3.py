d26083ca7bae5dae672b0288edd6ad5b632aa369
fegyiwnl <60046344+fegyiwnl@users.noreply.github.com>
2022-12-03 12:52:23 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,31 +1,48 @@
 def lol():
-    a = list(map(int, input().split()))  # задается переменная в которую записывается список - list(), затем, чтобы объединить 2 действия пишется map(), а в мап через запятую задаем эти аргументы, а именно int (эinteger) и input()(ввод), чтобы ввод был чрез пробелы пишется split() через точку рядом с input
-    k = len(a)  # длина строки или списка и тд
-    null_p = 0  # переменная к положительным
-    null_o = 0  # переменная к отрицательным
+    a = list(map(int, input("Введите числа в массив: ").split()))
+    k = len(a)
+    null_p = 0
+    null_o = 0
 
-    for i in range(1, k):  # цикл поиска первого положительного
-        if a[i] > 0:
-            null_p = i
-            break
-
-    for i in range(1, k):  # цикл поиска последнего отрицательного
-        if a[i] < 0:
-            null_o = i + 1
-
-    if null_p == 0 and a[0] > 0:
-        null_p = 1
-
-    if null_o == 0 and a[0] < 0:
-        null_o = 1
-
-    if null_p == 0 and null_o == 0:
-        return 0, 0
-    elif null_p == 0 and null_o > 0:
-        return 0, null_o
-    elif null_p >= 1 and null_o == 0:
-        return null_p, 0
+    if k == 1:
+        if a[0] > 0:
+            return 1, 0
+        elif a[0] < 0:
+            return 0, 1
+        else:
+            return 0, 0
+    elif k == 2:
+        if a[0] > 0:
+            if a[1] < 0:
+                return 1, 2
+            else:
+                return 1, 0
+        elif a[0] < 0:
+            if a[1] > 0:
+                return 2, 1
+            elif a[1] == 0:
+                return 0, 1
+        elif a[0] == 0:
+            if a[1] == 1:
+                return 2, 0
+            elif a[1] < 0:
+                return 0, 2
     else:
+        for i in range(k):
+            if a[i] > 0:
+                null_p = i
+                break
+        if a[0] > 0:
+            null_p = 1
+        elif null_p > 0:
+            null_p += 1
+
+        for i in range(k):
+            if a[i] < 0:
+                null_o = i
+        if null_o != 0:
+            null_o += 1
+
         return null_p, null_o
 
-print(lol())  # обязательно скобки
+print(lol())
